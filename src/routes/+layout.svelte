10e000e9512dd9bc702405328b0ffde708e7d0ca
sabinem <5292683+sabinem@users.noreply.github.com>
2025-05-04 06:53:27 +0200

    feat(layout): add basic layout to the site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
 	import '../app.css';
+	import Sidebar from '$lib/components/Sidebar.svelte';
+	import Header from '$lib/components/Header.svelte';
+
 	let { children } = $props();
 </script>
 
 <!-- Layout for the application -->
 <div class="grid h-screen grid-rows-[auto_1fr_auto]">
 	<!-- Header -->
-	<header>Chemboard</header>
+	<header>
+		<Header />
+	</header>
 	<!-- Grid Columns -->
 	<div class="grid grid-cols-1 md:grid-cols-[auto_1fr]">
 	  <!-- Left Sidebar. -->
-	  <aside class="p-4">(sidebar)</aside>
+	  <aside class="p-4">
+		<Sidebar />
+	  </aside>
 	  <!-- Main Content -->
 	  <main class="space-y-4 p-4">
 		{@render children()}
