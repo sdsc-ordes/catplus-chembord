b64d8ef9c5ad2828ea3bf89f8c490a7d827fb172
sabinem <5292683+sabinem@users.noreply.github.com>
2025-05-08 06:20:08 +0200

    feat(navigation): add navigation to the header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,13 +2,17 @@
 	import { AppBar } from '@skeletonlabs/skeleton-svelte';
 	import Paperclip from '@lucide/svelte/icons/paperclip';
 	import Calendar from '@lucide/svelte/icons/calendar';
+	import Home from '@lucide/svelte/icons/home';
+	import Database from '@lucide/svelte/icons/database';
 	import CircleUser from '@lucide/svelte/icons/circle-user';
 	import Search from '@lucide/svelte/icons/search';
 </script>
 
 <AppBar>
 	{#snippet lead()}
-	    <Search size={20} /> Search
+	    <a href="/"><Home size={20} />Home</a>
+	    <a href="/search"><Search size={20} />Search</a>
+		<a href="/explore"><Database size={20} />Data</a>
 	{/snippet}
 	{#snippet trail()}
 		<Paperclip size={20} />
