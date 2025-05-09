4de9ec4f7da6565ffd7f2a0e6de2ce58cce073b3
sabinem <5292683+sabinem@users.noreply.github.com>
2025-05-09 07:27:04 +0200

    style(homepage): add sidebar to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,28 @@
-<h1>Welcome at Cat+</h1>
-
-<div class="card w-full max-w-md preset-filled-surface-100-900 p-4 text-center">
-    <img src="https://www.epfl.ch/research/facilities/swisscat/wp-content/uploads/Labo-20240209-1920x1080.jpg" alt="SwissCat" class="rounded-lg" />
-    <p class="text-sm text-gray-500">SwissCat+ Lab, EPFL, Lausanne, Switzerland</p>
-</div>
-
-<div class="card w-full max-w-md preset-filled-surface-100-900 p-4 text-center">
-    <img src="https://www.epfl.ch/research/facilities/swisscat/wp-content/uploads/Schema-Mestrelab-e1684333653535.png" alt="SwissCat" class="rounded-lg" />
-    <p class="text-sm text-gray-500"> Datainfrastructure, EPFL, Lausanne, Switzerland</p>
+<div class="grid grid-cols-1 md:grid-cols-[auto_1fr]">
+	<aside class="items-start p-4">
+		<h1 class="mb-6 text-2xl font-bold text-gray-800">Cat Plus</h1>
+		<section class="space-y-4">
+			<div class="card preset-filled-surface-100-900 w-full max-w-md p-4 text-center">
+				<img
+					src="https://www.epfl.ch/research/facilities/swisscat/wp-content/uploads/Labo-20240209-1920x1080.jpg"
+					alt="SwissCat"
+					class="rounded-lg"
+				/>
+				<p class="text-sm text-gray-500">SwissCat+ Lab, EPFL, Lausanne, Switzerland</p>
+			</div>
+			<div class="card preset-filled-surface-100-900 w-full max-w-md p-4 text-center">
+				<img
+					src="https://www.epfl.ch/research/facilities/swisscat/wp-content/uploads/Schema-Mestrelab-e1684333653535.png"
+					alt="SwissCat"
+					class="rounded-lg"
+				/>
+				<p class="text-sm text-gray-500">Datainfrastructure of Cat Plus</p>
+			</div>
+		</section>
+	</aside>
+	<main>
+        <h1 class="mb-6 p-4 text-2xl font-bold text-gray-800">An automated Catalysis Lab</h1>
+        <p>Exlore the data</p>
+        <p>Search in the Campaigns</p>
+    </main>
 </div>
