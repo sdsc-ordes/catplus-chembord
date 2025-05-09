6b5e13e25fea5165349ad85b6cc53c2f64ffcb06
sabinem <5292683+sabinem@users.noreply.github.com>
2025-05-09 09:48:41 +0200

    refactor(home page): use ContentLayout component

--- src/lib/components/ContentLayout.svelte
+++ src/lib/components/ContentLayout.svelte
@@ -11,11 +11,11 @@
 
 <div class="grid grid-cols-1 md:grid-cols-[auto_1fr]">
 	<!-- Left Sidebar. -->
-	<aside class="bg-yellow-500 p-4">
+	<aside class="bg-tertiary-50 p-4">
 		{@render sidebar()}
 	</aside>
 	<!-- Main Content -->
-	<main class="space-y-4 bg-green-500 p-4">
+	<main class="space-y-4 p-4">
 		{@render main()}
 	</main>
 </div>
