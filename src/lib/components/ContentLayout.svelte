90a0cd8b5ad0f2061b6b39c80ebcf2c1361b1479
sabinem <5292683+sabinem@users.noreply.github.com>
2025-05-09 09:42:01 +0200

    style(Layout): add content layout as svelte component

---
+++ src/lib/components/ContentLayout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	let {
+		sidebar,
+		main
+	}: {
+		sidebar?: Snippet;
+		main?: Snippet;
+	} = $props();
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-[auto_1fr]">
+	<!-- Left Sidebar. -->
+	<aside class="bg-yellow-500 p-4">
+		{@render sidebar()}
+	</aside>
+	<!-- Main Content -->
+	<main class="space-y-4 bg-green-500 p-4">
+		{@render main()}
+	</main>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,15 +7,9 @@
 
 <!-- Layout for the application -->
 <div class="grid h-screen grid-rows-[auto_1fr_auto]">
-	<!-- Header -->
 	<header>
 		<Header />
 	</header>
-	<!-- Grid Columns -->
-	<div class="grid grid-cols-1 md:grid-cols-[auto_1fr] items-start">
-	    <!-- Main Content -->
-		{@render children()}
-	</div>
-	<!-- Footer -->
+	{@render children()}
 	<footer class="p-4 text-center">Chemboard 2025</footer>
-  </div>
+</div>
