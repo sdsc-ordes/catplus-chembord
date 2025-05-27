ec19178c19b6051102e08763f1099ae247a091ba
cmdoret <cyril.mattheydoret@gmail.com>
2025-05-27 15:56:34 +0200

    feat: use node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,10 +8,15 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// Using Node adapter for Kubernetes deployment
+		// See https://kit.svelte.dev/docs/adapter-node for more information
+		adapter: adapter({
+			// Default options
+			env: {
+				host: '0.0.0.0',
+				port: '3000'
+			}
+		})
 	}
 };
 
