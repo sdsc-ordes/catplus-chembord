ef6ace2f7453d617c6fc1f6ddc21b6820cbc24b1
sabinem <5292683+sabinem@users.noreply.github.com>
2025-07-29 17:00:27 +0200

    feat: show qlever errors on page

--- src/lib/components/ResultsHeaderSearch.svelte
+++ src/lib/components/ResultsHeaderSearch.svelte
@@ -8,6 +8,7 @@
         initialFilers,
         resultColumns,
 		query,
+        error,
 	} = $props();
 
 	// Encode the query string for display
@@ -16,36 +17,52 @@
 
 	let showQuery = $state(false);
 </script>
-
-<h1 class="mb-4 p-4 text-xl font-bold text-surface-800-200">
-    Results ({resultsTotal})
-</h1>
-<p>{initialFilers}</p>
-<p>{resultColumns}</p>
-<p class="text-sm">
-    You can execute the query at the QleverUI: be aware that the results will not be aggregated yet:
-</p>
-<div class="flex items-center justify-start space-x-6">
-    <a
-        href={qleverUrl}
-        target="_blank"
-        class="inline-flex items-center text-primary-500 hover:text-primary-600"
-    >
-        Copy query to Qlever UI
-        <ExternalLink class="ml-1 h-4 w-4" />
-    </a>
-    <div class="flex items-center space-x-2">
-        <input
-            type="checkbox"
-            id="show-query-checkbox"
-            bind:checked={showQuery}
-            class="checkbox bg-surface-50-950"
+{#if error}
+    <div class="card w-full preset-filled-error-100-900 p-4 text-center">
+        <p>{error}</p>
+    </div>
+{:else}
+    <!-- Main content area for displaying results -->
+    <div class="flex flex-col items-center justify-center p-4">
+        <ResultsHeaderSearch
+            resultsTotal={resultsTotal}
+            initialFilers={initialFilers}
+            resultColumns={resultColumns}
+            query={query}
+            showQuery={showQuery}
         />
-        <label for="show-query-checkbox" class="cursor-pointer text-sm">Show SPARQL Query</label>
     </div>
-</div>
-{#if query && showQuery}
-    <div class="text-xs font-mono bg-primary w-fit p-2">
-        <pre><code>{query}</code></pre>
+    <h1 class="mb-4 p-4 text-xl font-bold text-surface-800-200">
+        Results ({resultsTotal})
+    </h1>
+    <p>{initialFilers}</p>
+    <p>{resultColumns}</p>
+    <p class="text-sm">
+        You can execute the query at the QleverUI: be aware that the results will not be aggregated yet:
+    </p>
+    <div class="flex items-center justify-start space-x-6">
+        <a
+            href={qleverUrl}
+            target="_blank"
+            class="inline-flex items-center text-primary-500 hover:text-primary-600"
+        >
+            Copy query to Qlever UI
+            <ExternalLink class="ml-1 h-4 w-4" />
+        </a>
+        <div class="flex items-center space-x-2">
+            <input
+                type="checkbox"
+                id="show-query-checkbox"
+                bind:checked={showQuery}
+                class="checkbox bg-surface-50-950"
+            />
+            <label for="show-query-checkbox" class="cursor-pointer text-sm">Show SPARQL Query</label>
+        </div>
     </div>
+    {#if query && showQuery}
+        <div class="text-xs font-mono bg-primary w-fit p-2">
+            <pre><code>{query}</code></pre>
+        </div>
+    {/if}
 {/if}
+
